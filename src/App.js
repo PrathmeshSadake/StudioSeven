be10016f38c07e4201fcc559374d8e4915d109e8
Prathmesh Sadake <prathmeshsadake@gmail.com>
2021-02-27 19:51:28 +0530

    Added signIn route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from "react-router-dom";
 
 import Header from "./components/header/header";
 
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up";
 import Homepage from "./pages/homepage/homepage";
 import ShopPage from "./pages/shop/shop";
 import "./App.css";
@@ -14,6 +15,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signIn" component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
