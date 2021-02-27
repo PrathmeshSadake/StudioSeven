4900a05f9e620ceae34f0bc0b889f301bafd513e
Prathmesh Sadake <prathmeshsadake@gmail.com>
2021-02-27 19:52:08 +0530

    Rendering sign-in component

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./sign-in-and-sign-up.scss";
+
+import SignIn from "../../components/sign-in/sign-in";
+
+export default function SignInAndSignUpPage() {
+  return (
+    <div>
+      <SignIn />
+    </div>
+  );
+}
