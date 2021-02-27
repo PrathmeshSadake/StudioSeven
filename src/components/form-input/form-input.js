7e0c925afb8697efd9caddf5891bb52ff0be2d89
Prathmesh Sadake <prathmeshsadake@gmail.com>
2021-02-27 19:54:34 +0530

    Created custom input component

---
+++ src/components/form-input/form-input.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+import "./form-input.scss";
+
+export default function FormInput({ handleChange, label, ...otherProps }) {
+  return (
+    <div className="group">
+      <input
+        type="text"
+        className="form-input"
+        onChange={handleChange}
+        {...otherProps}
+      />
+      {label ? (
+        <label
+          className={`${
+            otherProps.value.length ? "shrink" : ""
+          } form-input-label`}
+        >
+          {label}
+        </label>
+      ) : null}
+    </div>
+  );
+}
