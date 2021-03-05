179c456f4f9948d17ec98c471d2c761905ba012e
Prathmesh Sadake <prathmeshsadake@gmail.com>
2021-03-05 23:05:38 +0530

    fixing button bug

--- src/components/sign-in/sign-in.js
+++ src/components/sign-in/sign-in.js
@@ -56,7 +56,7 @@ export default class SignIn extends Component {
           />
           <div className="buttons">
           <CustomButton type="submit">Sign in</CustomButton>
-          <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+          <CustomButton type="button" onClick={signInWithGoogle} isGoogleSignIn>
             {" "}
             Sign in with Google{" "}
           </CustomButton></div>
