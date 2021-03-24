5ff6714217955c1592c79ba7c236d5da57271c50
Prathmesh Sadake <prathmeshsadake@gmail.com>
2021-03-24 19:42:45 +0530

    redux changes

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 
 import { ReactComponent as Logo } from "../../assets/crown.svg";
-import {auth} from '../../firebase/firebase.utils'
+import { auth } from "../../firebase/firebase.utils";
 import "./header.scss";
 
-export default function Header({currentUser}) {
+function Header({ currentUser }) {
   return (
     <div className="header">
       <Link className="logo-container" to="/">
@@ -18,10 +19,22 @@ export default function Header({currentUser}) {
         <Link className="option" to="/contact">
           CONTACT
         </Link>
-        {
-          currentUser ? <div className='option' onClick={()=>auth.signOut()}>SIGN OUT</div> : <Link className='option' to='/signin'>SIGN IN</Link>
-        }
+        {currentUser ? (
+          <div className="option" onClick={() => auth.signOut()}>
+            SIGN OUT
+          </div>
+        ) : (
+          <Link className="option" to="/signin">
+            SIGN IN
+          </Link>
+        )}
       </div>
     </div>
   );
 }
+
+const mapStateToProps = (state) =>({
+  currentUser: state.user.currentUser
+})
+
+export default connect(mapStateToProps)(Header);
