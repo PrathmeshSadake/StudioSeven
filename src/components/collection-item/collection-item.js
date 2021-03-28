7db83856eca69ad34b063e9cca9ce21d9972d6f2
Prathmesh Sadake <prathmeshsadake@gmail.com>
2021-03-28 21:23:11 +0530

    creating the cart reducer

--- src/components/collection-item/collection-item.js
+++ src/components/collection-item/collection-item.js
@@ -1,8 +1,11 @@
 import React from "react";
+import { connect } from "react-redux",
 import "./collection-item.scss";
 
-export default function CollectionItem({ id, name, price, imageUrl }) {
-  return (
+import CustomButton from "../custom-button/custom-button";
+import {addItem} from '../../redux/cart/cart.actions';
+
+const CollectionItem = ({ id, name, price, imageUrl, addItem }) =>(
     <div className="collection-item">
       <div
         className="image"
@@ -14,6 +17,12 @@ export default function CollectionItem({ id, name, price, imageUrl }) {
         <span className="name">{name}</span>
         <div className="price">{price}</div>
       </div>
+      <CustomButton inverted>Add to cart</CustomButton>
     </div>
   );
-}
+
+  const mapDispatchToProps = dispatch => ({
+    addItem: item => dispatch(addItem(item))
+  });
+
+  export default connect(null ,mapDispatchToProps)(CollectionItem);
--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -1,5 +1,6 @@
 const CartActionTypes = {
   TOGGLE_HIDDEN_STATE: "TOGGLE_HIDDEN_STATE",
+  ADD_ITEM: "ADD_ITEM",
 };
 
 export default CartActionTypes;
