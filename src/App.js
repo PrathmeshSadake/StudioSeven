fd76358959be08e8c2d590fd87bd3c35d16265fb
Prathmesh Sadake <prathmeshsadake@gmail.com>
2021-07-12 01:52:11 +0530

    new project setup

--- src/App.js
+++ src/App.js
@@ -1,74 +1,25 @@
-import React from "react";
-import { Route, Switch, Redirect } from "react-router-dom";
-import { connect } from "react-redux";
-import { setCurrentUser } from "./redux/user/user.actions";
+import logo from './logo.svg';
+import './App.css';
 
-import Header from "./components/header/header";
-
-import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up";
-import Homepage from "./pages/homepage/homepage";
-import ShopPage from "./pages/shop/shop";
-import "./App.css";
-
-import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
-
-class App extends React.Component {
-  unsubscribeFromAuth = null;
-
-  componentDidMount() {
-    const { setCurrentUser } = this.props;
-
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
-      if (userAuth) {
-        const userRef = await createUserProfileDocument(userAuth);
-
-        userRef.onSnapshot((snapshot) => {
-          // console.log(snapshot.data());
-          setCurrentUser({
-            id: snapshot.id,
-            ...snapshot.data(),
-          });
-          // console.log(this.state);
-        });
-      }
-      setCurrentUser(userAuth);
-    });
-  }
-
-  componentWillUnmount() {
-    this.unsubscribeFromAuth();
-  }
-
-  render() {
-    return (
-      <div>
-        <Header />
-        <Switch>
-          <Route exact path="/" component={Homepage} />
-          <Route path="/shop" component={ShopPage} />
-          <Route
-            exact
-            path="/signIn"
-            render={() =>
-              this.props.currentUser ? (
-                <Redirect to="/" />
-              ) : (
-                <SignInAndSignUpPage />
-              )
-            }
-          />
-        </Switch>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-const mapStateToProps = ({ user }) => ({
-  currentUser: user.currentUser,
-});
-
-const mapDispatchToProps = (dispatch) => ({
-  setCurrentUser: (user) => dispatch(setCurrentUser(user)),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
--- src/redux/cart/cart.types.js
+++
@@ -1,6 +0,0 @@
-const CartActionTypes = {
-  TOGGLE_HIDDEN_STATE: "TOGGLE_HIDDEN_STATE",
-  ADD_ITEM: "ADD_ITEM",
-};
-
-export default CartActionTypes;
