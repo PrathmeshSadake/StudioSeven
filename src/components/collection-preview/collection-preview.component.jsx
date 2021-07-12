849b52472093ed9d69437ab99d01edcbd74d701f
Prathmesh Sadake <prathmeshsadake@gmail.com>
2021-07-12 15:26:34 +0530

    collection preview component

---
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import CollectionItem from "../collection-item/collection-item.component";
+
+import "./collection-preview.styles.scss";
+
+export default function CollectionPreview({ title, items }) {
+  return (
+    <div className="collection-preview">
+      <h1>{title.toUpperCase()}</h1>
+      <div className="preview">
+        {items
+          .filter((item, idx) => idx < 4)
+          .map(({ id, ...otherItemProps }) => (
+            <CollectionItem key={id} {...otherItemProps} />
+          ))}
+      </div>
+    </div>
+  );
+}
