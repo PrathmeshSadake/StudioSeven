4e76d53e9946149d16da56ec960b048a02220c17
Prathmesh Sadake <prathmeshsadake@gmail.com>
2021-07-16 18:58:00 +0530

    created memoized selectors

---
+++ src/redux/cart/cart.selectors.js
@@ -0,0 +1,21 @@
+import { createSelector } from "reselect";
+// SELECTORS prevent re-rendering of components when state change is not related to the component
+
+const selectCart = (state) => state.cart;
+
+// returns cartItems
+export const selectCartItems = createSelector(
+  [selectCart],
+  (cart) => cart.cartItems
+);
+
+// returns total count of items in cart
+export const selectCartItemsCount = createSelector(
+  [selectCartItems],
+  (cartItems) =>
+    cartItems.reduce(
+      (accumulatedQuantity, cartItem) =>
+        accumulatedQuantity + cartItem.quantity,
+      0
+    )
+);
