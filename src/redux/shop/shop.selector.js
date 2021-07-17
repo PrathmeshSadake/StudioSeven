3dc0bc1205896b36dfd0a14658e5b8e809761f2e
Prathmesh Sadake <prathmeshsadake@gmail.com>
2021-07-17 12:17:37 +0530

    removed unwanted code

--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -1,13 +1,5 @@
 import { createSelector } from "reselect";
 
-const COLLECTION_ID_MAP = {
-  hats: 100,
-  sneakers: 200,
-  jackets: 300,
-  women: 400,
-  men: 500,
-};
-
 const selectShop = (state) => state.shop;
 
 export const selectCollections = createSelector(
