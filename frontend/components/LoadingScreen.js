a24a9ff263ad6e62afa8fb96e91f77f9191f9507
Prathmesh Sadake <prathmeshsadake@gmail.com>
2023-03-09 19:44:07 +0530

    Created a LoadingScreen

---
+++ frontend/components/LoadingScreen.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { LineWave } from "react-loader-spinner";
+
+const LoadingScreen = () => {
+  return (
+    <div className='h-screen flex justify-center items-center'>
+      <LineWave
+        height='100'
+        width='100'
+        color='#4fa94d'
+        ariaLabel='line-wave'
+        wrapperStyle={{}}
+        wrapperClass=''
+        visible={true}
+      />
+    </div>
+  );
+};
+
+export default LoadingScreen;
