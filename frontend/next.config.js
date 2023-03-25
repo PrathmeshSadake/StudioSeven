510c47b8d0630f7bdc03e6dc02f2f5c0819c40b9
Prathmesh Sadake <prathmeshsadake@gmail.com>
2023-03-25 16:12:37 +0530

    cra and mui setup created navbar

--- frontend/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  async redirects() {
-    return [{ source: "/cancelled", destination: "/", permanent: true }];
-  },
-};
-
-module.exports = nextConfig;
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
