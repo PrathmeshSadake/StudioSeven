6df4a00d048a1f8ae02e0551bfbd80f601ebcc1a
Prathmesh Sadake <prathmeshsadake@gmail.com>
2023-05-04 23:15:01 +0530

    created Navbar and layout components

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,25 @@
-import '@/styles/globals.css'
+import { SessionProvider } from "next-auth/react";
+import Layout from "@/components/Layout";
+import "@/styles/globals.css";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
+  subsets: ["latin"],
+});
+
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
+  return (
+    <SessionProvider session={session}>
+      <main className={poppins.className}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </main>
+    </SessionProvider>
+  );
 }
