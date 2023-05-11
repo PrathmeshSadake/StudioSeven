59c1bf9ba387f478c0a8424b180fcd16b5cb6cfd
Prathmesh Sadake <prathmeshsadake@gmail.com>
2023-05-12 00:21:02 +0530

    created zustand store and add-product and remove-product

--- src/store.js
+++ src/store.js
@@ -5,9 +5,48 @@ const useCartStore = create(
   persist(
     (set) => ({
       cartItems: [],
-      // increaseCartItem: () => set((state) => ({ bears: state.bears + 1 })),
-      // decreaseCartItem: () => set((state) => ({ bears: state.bears + 1 })),
-      removeAllCartItems: () => set({ cartItems: [] }),
+      addProduct: (item) =>
+        set((state) => {
+          const existingItem = state.cartItems.find(
+            (product) => product.id === item.id
+          );
+          if (existingItem) {
+            const updatedCartItems = state.cartItems.map((cartItem) => {
+              if (cartItem.id === item.id) {
+                return { ...cartItem, quantity: cartItem.quantity + 1 };
+              }
+              return cartItem;
+            });
+            return { cartItems: updatedCartItems };
+          } else {
+            return {
+              cartItems: [...state.cartItems, { ...item, quantity: 1 }],
+            };
+          }
+        }),
+      removeProduct: (item) =>
+        set((state) => {
+          const existingItem = state.cartItems.find(
+            (product) => product.id === item.id
+          );
+          if (existingItem && existingItem.quantity > 1) {
+            const updatedCartItems = state.cartItems.map((cartItem) => {
+              if (cartItem.id === item.id) {
+                return {
+                  ...cartItem,
+                  quantity: cartItem.quantity > 0 ? cartItem.quantity - 1 : 0,
+                };
+              }
+              return cartItem;
+            });
+            return { cartItems: updatedCartItems };
+          } else {
+            const filteredCartItems = state.cartItems.filter(
+              (cartItem) => cartItem.id !== item.id
+            );
+            return { cartItems: filteredCartItems };
+          }
+        }),
     }),
 
     {
