483fbc9eeb6b9505702ac83b71815754b4dfc5f9
Prathmesh Sadake <prathmeshsadake@gmail.com>
2023-07-12 23:16:28 +0530

    Added button for testing

---
+++ admin/app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+const Home = () => {
+  return (
+    <div>
+      <p>Hello Admin Dashboard!</p>
+      <Button variant='outline'>Button</Button>
+    </div>
+  );
+};
+
+export default Home;
