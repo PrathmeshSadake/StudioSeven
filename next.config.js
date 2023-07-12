fc9545bedf73a227e2a613f543f0b03cb0508ce2
Prathmesh Sadake <prathmeshsadake@gmail.com>
2023-07-12 22:53:47 +0530

    Initialized admin project

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ["lh3.googleusercontent.com", "files.stripe.com"],
-  },
-};
-
-module.exports = nextConfig;
--- src/utils/priceFormat.js
+++
@@ -1,8 +0,0 @@
-const formatPrice = (amount) => {
-  return new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "INR",
-  }).format(amount / 100);
-};
-
-export default formatPrice;
